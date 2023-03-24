37c093ac5c88bd3affbffe5047d79cef8de2f3db
vasco sousa <vasco.alex.sousa@gmail.com>
2023-03-24 18:42:02 +0000

    correction constant

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,6 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = ( { isMobile }) => {
-  const computer = useGLTF('../desktop_pc/scene.gltf')
   const computer = useGLTF('../../assets/desktop_pc/scene.gltf')
 
   return (
