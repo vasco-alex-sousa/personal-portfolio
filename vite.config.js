5134ae505a189f2472734dc872f295e79168634f
vasco sousa <vasco.alex.sousa@gmail.com>
2023-03-24 00:39:20 +0000

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: path.join(__dirname, 'build'),
+  }
 })
